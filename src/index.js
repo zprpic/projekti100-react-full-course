2bd16855589b19c6049a2e0a8f72fc95ac43ab91
zprpic <prpiczvonimirprpiczvonimir@gmail.com>
2021-08-16 16:24:08 +0200

    Named exports

--- src/index.js
+++ src/index.js
@@ -1,29 +1,10 @@
 import React from "react";
 import reactDom from "react-dom";
 
+import { books } from "./books";
+
 //CSS
 import "./index.css";
-
-const books = [
-  {
-    id: 1,
-    author: "Amelia Hepworth",
-    title: "I Love You to the Moon and Back",
-    img: "https://images-na.ssl-images-amazon.com/images/I/517h-u1AQlL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
-  },
-  {
-    id: 2,
-    author: "Dr. Seuss",
-    title: "Oh, the Places You'll Go!",
-    img: "https://images-na.ssl-images-amazon.com/images/I/51VU-ask3QS._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
-  },
-  {
-    id: 3,
-    author: " Bill Martin Jr. / Eric Carle",
-    title: "Brown Bear, Brown Bear, What Do You See?",
-    img: "https://images-na.ssl-images-amazon.com/images/I/51430n+9jlL._SY344_BO1,204,203,200_.jpg",
-  },
-];
 
 const BookList = () => {
   return (
