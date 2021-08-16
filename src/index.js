1c44e57a40a380ed4e4ca55c9297b58490560e54
zprpic <prpiczvonimirprpiczvonimir@gmail.com>
2021-08-16 15:50:41 +0200

    Key prop and spread operator...

--- src/index.js
+++ src/index.js
@@ -6,16 +6,19 @@ import "./index.css";
 
 const books = [
   {
+    id: 1,
     author: "Amelia Hepworth",
     title: "I Love You to the Moon and Back",
     img: "https://images-na.ssl-images-amazon.com/images/I/517h-u1AQlL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
   },
   {
+    id: 2,
     author: "Dr. Seuss",
     title: "Oh, the Places You'll Go!",
     img: "https://images-na.ssl-images-amazon.com/images/I/51VU-ask3QS._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
   },
   {
+    id: 3,
     author: " Bill Martin Jr. / Eric Carle",
     title: "Brown Bear, Brown Bear, What Do You See?",
     img: "https://images-na.ssl-images-amazon.com/images/I/51430n+9jlL._SY344_BO1,204,203,200_.jpg",
@@ -26,15 +29,14 @@ const BookList = () => {
   return (
     <section className="booklist">
       {books.map((book) => {
-        const { author, title, img } = book;
-        return <Book book={book} />;
+        return <Book key={book.id} {...book} />;
       })}
     </section>
   );
 };
 
 const Book = (props) => {
-  const { img, title, author } = props.book;
+  const { img, title, author } = props;
   return (
     <article className="book">
       <img src={img} alt="Book"></img>
