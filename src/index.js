c1cd58dae1b26799ae66ee4fbd88c205cdf63177
zprpic <prpiczvonimirprpiczvonimir@gmail.com>
2021-08-16 12:01:55 +0200

    Nesting components...

--- src/index.js
+++ src/index.js
@@ -2,7 +2,18 @@ import React from "react";
 import reactDom from "react-dom";
 
 const Greeting = () => {
-  return <h1>hello world</h1>;
+  return (
+    <div>
+      <Person />
+      <Message />
+    </div>
+  );
+};
+
+const Person = () => <h2>prle</h2>;
+
+const Message = () => {
+  return <p>para para paragraph</p>;
 };
 
 reactDom.render(<Greeting />, document.getElementById("root"));
