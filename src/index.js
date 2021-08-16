6627509b43dea7b0c722e29d4b5097e377ede57e
zprpic <prpiczvonimirprpiczvonimir@gmail.com>
2021-08-16 15:37:25 +0200

    Iterating over an array of books to render data

--- src/index.js
+++ src/index.js
@@ -4,37 +4,37 @@ import reactDom from "react-dom";
 //CSS
 import "./index.css";
 
-const firstBook = {
-  author: "Amelia Hepworth",
-  title: "I Love You To The Moon And Back",
-  img: "https://images-na.ssl-images-amazon.com/images/I/517h-u1AQlL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
-};
-
-const secondBook = {
-  author: "Dr. Seuss",
-  title: "Oh, the Places You'll Go!",
-  img: "https://images-na.ssl-images-amazon.com/images/I/51VU-ask3QS._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
-};
+const books = [
+  {
+    author: "Amelia Hepworth",
+    title: "I Love You to the Moon and Back",
+    img: "https://images-na.ssl-images-amazon.com/images/I/517h-u1AQlL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
+  },
+  {
+    author: "Dr. Seuss",
+    title: "Oh, the Places You'll Go!",
+    img: "https://images-na.ssl-images-amazon.com/images/I/51VU-ask3QS._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
+  },
+  {
+    author: " Bill Martin Jr. / Eric Carle",
+    title: "Brown Bear, Brown Bear, What Do You See?",
+    img: "https://images-na.ssl-images-amazon.com/images/I/51430n+9jlL._SY344_BO1,204,203,200_.jpg",
+  },
+];
 
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      />
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
+      {books.map((book) => {
+        const { author, title, img } = book;
+        return <Book book={book} />;
+      })}
     </section>
   );
 };
 
-const Book = ({ img, title, author }) => {
-  //const { img, title, author } = props;
+const Book = (props) => {
+  const { img, title, author } = props.book;
   return (
     <article className="book">
       <img src={img} alt="Book"></img>
