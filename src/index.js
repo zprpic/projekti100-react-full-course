2ca63f65ba2c7e6e21f86a4937614904311022f7
zprpic <prpiczvonimirprpiczvonimir@gmail.com>
2021-08-16 14:02:38 +0200

    Playing around with props and destructuring

--- src/index.js
+++ src/index.js
@@ -1,41 +1,47 @@
 import React from "react";
 import reactDom from "react-dom";
 
+//CSS
+import "./index.css";
+
+const firstBook = {
+  author: "Amelia Hepworth",
+  title: "I Love You To The Moon And Back",
+  img: "https://images-na.ssl-images-amazon.com/images/I/517h-u1AQlL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
+};
+
+const secondBook = {
+  author: "Dr. Seuss",
+  title: "Oh, the Places You'll Go!",
+  img: "https://images-na.ssl-images-amazon.com/images/I/51VU-ask3QS._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
+};
+
 const BookList = () => {
   return (
-    <section>
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+    <section className="booklist">
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
     </section>
   );
 };
 
-const Book = () => {
+const Book = ({ img, title, author }) => {
+  //const { img, title, author } = props;
   return (
-    <article>
-      <Image />
-      <Title />
-      <Author />
+    <article className="book">
+      <img src={img} alt="Book"></img>
+      <h1>{title}</h1>
+      <h4>{author}</h4>
     </article>
   );
-};
-
-const Image = () => (
-  <img
-    src="https://images-na.ssl-images-amazon.com/images/I/517h-u1AQlL._SX482_BO1,204,203,200_.jpg"
-    alt="Book image"
-  ></img>
-);
-
-const Title = () => {
-  return <h1>I Love You To The Moon And Back</h1>;
-};
-
-const Author = () => {
-  return <h4>Amelia Hepworth</h4>;
 };
 
 reactDom.render(<BookList />, document.getElementById("root"));
