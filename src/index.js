9ccac082255dd6edcb55e9bce37e4dad01e5d741
zprpic <prpiczvonimirprpiczvonimir@gmail.com>
2021-08-16 16:15:21 +0200

    Difference between reference and more complex events... Immediate invocation vs passing to an arrow function

--- src/index.js
+++ src/index.js
@@ -35,13 +35,26 @@ const BookList = () => {
   );
 };
 
-const Book = (props) => {
-  const { img, title, author } = props;
+const Book = ({ author, title, img }) => {
+  //attribute, eventHandler
+  //onClick, onMouseOver
+  const clickHandler = () => {
+    alert("hello world!");
+  };
+  const complexExample = (author) => {
+    console.log(author);
+  };
   return (
     <article className="book">
       <img src={img} alt="Book"></img>
-      <h1>{title}</h1>
+      <h1 onClick={() => console.log(title)}>{title}</h1>
       <h4>{author}</h4>
+      <button type="button" onClick={clickHandler}>
+        reference example
+      </button>
+      <button type="button" onClick={() => complexExample(author)}>
+        more complex example
+      </button>
     </article>
   );
 };
